edfc13a9544de4e3a1618433aaefa4887a51c421
Logan <logankrieger317@gmail.com>
2024-01-03 13:57:44 -0600

    minor change

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,8 +4,8 @@ export default function Book() {
     return(
         <div>
 
-        <h1 className="text-4xl font-bold text-center">About</h1>
-        <p className="text-center">This is the about page</p>
+        <h1 className="text-4xl font-bold text-center">Book</h1>
+        <p className="text-center">This is the booking page</p>
 
         </div>    
         
